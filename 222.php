3567507aa3d0fc38d0dcd0ff4b5b8fb7a9799e06
Artem Lipodat <a.lipodat@mail.ru>
2022-11-25 08:50:14 +0400

    fix

---
+++ 222.php
@@ -0,0 +1,84 @@
+<?php include_once('./assets/templates/header.php'); ?>
+
+<div class="breadcrumbs mt-4">
+  <div class="container">
+    <ul class="d-flex justify-content-end">
+      <li><a href="/">Главная</a></li>
+      <li>/</li>
+      <li>Экспозиционно-тематические мероприятия</li>
+    </ul>
+  </div>
+</div>
+<main>
+  <div class="container">
+    <h1>Экспозиционно-тематические мероприятия</h1>
+  </div>
+  <section class="mero mt-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <a href=""><img src="./assets/img/mero__1.png"></a>
+        </div>
+        <div class="col-6">
+          <a href=""><img src="./assets/img/mero__2.png"></a>
+        </div>
+        <div class="col-6">
+          <a href=""><img src="./assets/img/mero__3.png"></a>
+        </div>
+        <div class="col-6">
+          <a href=""><img src="./assets/img/mero__4.png"></a>
+        </div>
+      </div>
+      <h3>Преимущества сотрудничества с АСМАП-Сервис:</h3>
+      <ul>
+        <li>минимальный пакет документов</li>
+        <li>минимальные сроки оформления</li>
+        <li>полностью дистанционное оформление (при условии прохождения процедуры дактилоскопии)</li>
+        <li>высококвалифицированные специалисты</li>
+        <li>доступная цена</li>
+      </ul>
+      <a>
+        <img src="./assets/img/reklama__b.png">
+      </a>
+      <a class="d-block" href="">
+        <img src="./assets/img/reklama_z.png">
+      </a>
+    </div>
+  </section>
+  <section class="news">
+    <div class="title_bg">
+      <div class="container">
+        <h3 class="ps-5 title h3">Новости</h3>
+      </div>
+    </div>
+    <div class="container">
+      <div class="p-5">
+        <div class="row">
+          <div class="col-3">
+            <span class="news__date">22.04.2022</span>
+            <p class="news__prev mt-3">Напоминаем о необходимости заблаговременной оплаты в связи с переносе выходных дней</p>
+            <a href="" class="button mt-4">Подробнее</a>
+          </div>
+          <div class="col-3">
+            <span class="news__date">22.04.2022</span>
+            <p class="news__prev mt-3">Напоминаем о необходимости заблаговременной оплаты в связи с переносе выходных дней</p>
+            <a href="" class="button mt-4">Подробнее</a>
+          </div>
+          <div class="col-3">
+            <span class="news__date">22.04.2022</span>
+            <p class="news__prev mt-3">Напоминаем о необходимости заблаговременной оплаты в связи с переносе выходных дней</p>
+            <a href="" class="button mt-4">Подробнее</a>
+          </div>
+          <div class="col-3">
+            <span class="news__date">22.04.2022</span>
+            <p class="news__prev mt-3">Напоминаем о необходимости заблаговременной оплаты в связи с переносе выходных дней</p>
+            <a href="" class="button mt-4">Подробнее</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+
+<?php include_once('./assets/templates/footer.php'); ?>
---
+++ reklama.php
@@ -0,0 +1,67 @@
+<?php include_once('./assets/templates/header.php'); ?>
+
+<div class="breadcrumbs mt-4">
+  <div class="container">
+    <ul class="d-flex justify-content-end">
+      <li><a href="/">Главная</a></li>
+      <li>/</li>
+      <li>Реклама и сотрудничество</li>
+    </ul>
+  </div>
+</div>
+<main>
+  <div class="container">
+    <h1>Реклама и сотрудничество</h1>
+  </div>
+  <section class="reklama mt-4">
+    <div class="container">
+      <h3>Преимущества сотрудничества с АСМАП-Сервис:</h3>
+      <ul>
+        <li>Размещение баннеров на сайте</li>
+        <li>Размещение баннеров на выставках</li>
+        <li>Размещение баннеров в печатных изданиях</li>
+        <li>Сувенирная продукция</li>
+        <li>Доступная цена</li>
+      </ul>
+      <a>
+        <img src="./assets/img/reklama__b.png">
+      </a>
+      <a class="d-block" href="">
+        <img src="./assets/img/reklama_z.png">
+      </a>
+    </div>
+  </section>
+  <section class="reklama__2 mt-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div class="reklama__title">
+            Размещение баннеров на сайте
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum. Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum.</p>
+        </div>
+        <div class="col-6">
+          <div class="reklama__title">
+            Размещение баннеров на выставках
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum. Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum.</p>
+        </div>
+        <div class="col-6">
+          <div class="reklama__title">
+            Размещение баннеров в печатных изданиях
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum. Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum.</p>
+        </div>
+        <div class="col-6">
+          <div class="reklama__title">
+            Сувенирная продукция
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum. Lorem ipsum dolor sit amet consectetur. Mauris aliquet lacus ante eleifend lectus sapien dictum bibendum.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+
+<?php include_once('./assets/templates/footer.php'); ?>
